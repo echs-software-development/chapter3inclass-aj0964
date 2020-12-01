44535a58b81effb6b7fd6f723eca5a01dc5f793e
whack <aj0964@students.necsd.net>
2020-12-01 15:18:48 +0000

    prob1 and 2

--- Prob1.java
+++ Prob1.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 
 public class Prob1{
   public static void main(String[] args){
-    
-
+    Scanner Scan = new Scanner(System.in);
+    System.out.print("Enter two intergers:");
+    int number1 = Scan.nextInt();
+    int number2 = Scan.nextInt();
+    if (number1 == number2)
+    System.out.println("Yes");
 
     
   }
--- Prob2.java
+++ Prob2.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Prob2  {
   public static void main(String[] args){
-    
-
+    Scanner Scan = new Scanner(System.in);
+    System.out.print("Enter First number: ");
+    double num1 = Scan.nextDouble();
+    System.out.print("Enter Second number: ");
+    double num2 = Scan.nextDouble();
+    System.out.print("Enter Third number: ");
+    double num3 = Scan.nextDouble();
+    System.out.print("Enter Fourth number: ");
+    double num4 = Scan.nextDouble();
+    double avg = (num1 + num2 + num3 + num4);
+    avg = (avg / 4);
+    if (avg >= 89.5)
+    System.out.println("Above average");
 
     
   }
