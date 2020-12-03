af2f2cdbfc776586ebc6ee85ee2fd2487b527a96
whack <aj0964@students.necsd.net>
2020-12-03 15:17:42 +0000

    problem 4

--- Prob4.java
+++ Prob4.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Prob4  {
   public static void main(String[] args){
-    
-
+  Scanner scan = new Scanner(System.in);
+  System.out.print("What is the temperature?: ");    
+  double temp = scan.nextDouble();
+  if (temp >= 102 || <= 99)
+   System.out.println("WARNING");
+  
+  else 
+   System.out.println("Temperature is ok.");  
+  
 
     
   }
