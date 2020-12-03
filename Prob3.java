651aa69b90939d460a142160a84014c64d06edcf
whack <aj0964@students.necsd.net>
2020-12-03 15:03:48 +0000

    problem 3

--- Prob3.java
+++ Prob3.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class Prob3  {
   public static void main(String[] args){
-    
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Give a number, 0-4:");
+    int grade = scan.nextInt();
+    if (grade == 4)
+      System.out.println("A");
+    else if(grade == 3)
+      System.out.println("B");
+    else if(grade == 2)
+      System.out.println("C");
+    else (grade == 1);
+      System.out.println("F");
 
 
     
